3c29f9656160380f2307a3f149c87649403989e4
woosub <98096591+Ppp5500@users.noreply.github.com>
2022-05-23 11:28:23 +0900

    Flip_Test
    시작화면을 만들고 메인씬으로 가는 버튼과 게임종료 버튼을 만들었습니다.
    테스트용 이미지를 만들어서 메인씬에서 카드 인식 테스트를 했습니다.
    이미지 타겟의 Default Trackable Event 컴포넌트에서 On Target Found를 통해 MainGameManager에 현재 인식되고 있는 카드를 string 형태로 전달합니다.

---
+++ Assets/Scripts/MainGameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class MainGameManager : MonoBehaviour
+{
+    enum NowState
+    {
+        Player01Turn,
+        Player02Turn,
+        Result
+    }
+    private NowState nowState;
+
+    public Text nowStateText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nowState = NowState.Player01Turn;
+    }
+
+    public void FilpCard(string _inputCard)
+    {
+        nowStateText.text = _inputCard;
+    }
+}
